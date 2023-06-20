5384a76e9ea3d384c69bdec813eadf595fec0fdd
wechuli <wechulipaul@yahoo.com>
2023-06-20 09:20:44 +0300

    basic actions setup

---
+++ src/utils/test.ts
@@ -0,0 +1,10 @@
+import si from "systeminformation";
+
+si.cpu().then((data) => console.log(data));
+
+// memory utilization
+si.mem().then((data) => console.log(data));
+
+// disk utilization
+
+si.diskLayout().then((data) => console.log(data));
