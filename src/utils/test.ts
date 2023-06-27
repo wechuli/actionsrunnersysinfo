ecced6c2b0dab97bcfab2080555907db5ae6b796
wechuli <wechulipaul@yahoo.com>
2023-06-27 09:10:27 +0300

    adding basic logic

--- src/utils/test.ts
+++ src/utils/test.ts
@@ -8,3 +8,7 @@ si.mem().then((data) => console.log(data));
 // disk utilization
 
 si.diskLayout().then((data) => console.log(data));
+
+// check cpu usage
+
+si.currentLoad().then((data) => console.log(data));
