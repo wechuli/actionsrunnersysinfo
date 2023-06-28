d869c8bde68bbf5c86a52fd3e6e1761877613613
wechuli <wechulipaul@yahoo.com>
2023-06-28 16:08:59 +0300

    cahnge main

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import * as core from "@actions/core";
 async function run(): Promise<void> {
   try {
     console.log("Hello World");
+    console.log(core.getState("isPost"));
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message);
   }
