aa3025dfa483033656f105dbd181e7b2eb715a0f
wechuli <wechulipaul@yahoo.com>
2023-06-28 16:05:51 +0300

    adding website basic logic

---
+++ website/src/app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam a at sunt
+        sed voluptas recusandae pariatur sit! Consequatur quibusdam qui fugit
+        beatae esse modi, ipsa quasi voluptas magnam excepturi aspernatur.
+      </p>
+    </main>
+  );
+}
