58ad08e2733cc214455c6f987da76a25dacb6f43
wechuli <wechulipaul@yahoo.com>
2023-06-29 09:15:48 +0300

    move error handling to the actual functions

--- src/main.ts
+++ src/main.ts
@@ -2,25 +2,35 @@ import * as core from "@actions/core";
 import { IsPost, IsPre, State } from "./utils/state-helper";
 
 async function setup(): Promise<void> {
-  console.log("I am the setup function");
-}
-async function upload(): Promise<void> {
-  console.log("I am the upload function");
+  try {
+    console.log("I am the setup function");
+  } catch (error) {
+    if (error instanceof Error) {
+      core.setFailed(error.message);
+    }
+  } finally {
+    core.saveState(State.IsPre, "false");
+  }
 }
 async function run(): Promise<void> {
   try {
     console.log("I am the main function");
   } catch (error) {
-    if (error instanceof Error) core.setFailed(error.message);
+    if (error instanceof Error) {
+      core.setFailed(error.message);
+    }
+  } finally {
+    core.saveState(State.IsPost, "true");
   }
+}
+async function upload(): Promise<void> {
+  console.log("I am the upload function");
 }
 
 if (IsPre()) {
   setup();
-  core.saveState(State.IsPre, "false");
 } else if (!IsPre() && !IsPost()) {
   run();
-  core.saveState(State.IsPost, "true");
 } else {
   upload();
 }
