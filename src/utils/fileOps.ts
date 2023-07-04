0f226e4238491a9b2ad1be49d71c51cfb11eab6c
wechuli <wechulipaul@yahoo.com>
2023-07-04 08:16:36 +0300

    get correct endpoint

--- src/utils/fileOps.ts
+++ src/utils/fileOps.ts
@@ -1,10 +1,12 @@
 import fs from "fs";
+import path from "path";
 
 // create empty JSON file
 export function createJSONfile(): void {
   let fileLocation = process.env["RUNNER_TEMP"];
+  console.log(fileLocation);
   if (fileLocation) {
-    fileLocation += "\\sysinfo.json";
+    fileLocation = path.join(fileLocation, "sysinfo.json");
     fs.writeFileSync(fileLocation, "{}");
   }
 }
