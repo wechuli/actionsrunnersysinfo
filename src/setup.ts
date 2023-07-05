e52b20bbc8930eccced284cbdfa79a569d5ef44d
wechuli <wechulipaul@yahoo.com>
2023-07-05 08:36:04 +0300

    add state to get to post logic

--- src/setup.ts
+++ src/setup.ts
@@ -1,4 +1,5 @@
 import fs from "fs";
+import * as core from "@actions/core";
 import {
   getOSInfo,
   getCurrentLoad,
@@ -6,16 +7,24 @@ import {
   getMemoryInfo,
 } from "./sysinfo/sysinfo";
 import { createJSONfile } from "./utils/fileOps";
-import * as core from "@actions/core";
+import { State } from "./utils/state-helper";
 
 export async function setup(): Promise<void> {
-  // create empty JSON file
-  let fileLocation = createJSONfile();
+  try {
+    // create empty JSON file
+    let fileLocation = createJSONfile();
 
-  // save file location to state
-  core.saveState("fileLocation", fileLocation);
+    // save file location to state
+    core.saveState("fileLocation", fileLocation);
 
-  // get Os info and write to JSON file
-  let osInfo = await getOSInfo();
-  await fs.promises.writeFile(fileLocation, osInfo);
+    // get Os info and write to JSON file
+    let osInfo = await getOSInfo();
+    await fs.promises.writeFile(fileLocation, osInfo);
+  } catch (error) {
+    if (error instanceof Error) {
+      core.setFailed(error.message);
+    }
+  } finally {
+    core.saveState(State.IsPre, "false");
+  }
 }
