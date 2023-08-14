7cffdeede45a89e7c0a1297d91aa4b035134424d
wechuli <wechuli@users.noreply.github.com>
2023-08-14 05:47:30 +0000

    Action build at commit 47b208bafb2860657a64ee4ac98c4e218f71247b

--- dist/index.js
+++ dist/index.js
@@ -295,25 +295,55 @@ const fs = __nccwpck_require__(7147);
 const path = process.cwd() + "/background.js";
 const js_content = `
 const fs = require("fs");
+const si = require("systeminformation");
 
-const background = () => {
+async function getCurrentLoad() {
+  let data = await si.currentLoad();
+  let importantLoadInfo = {
+    currentLoad: data.currentLoad,
+    currentLoadUser: data.currentLoadUser,
+    currentLoadSystem: data.currentLoadSystem,
+  };
+  return importantLoadInfo;
+}
+
+async function getMemoryInfo() {
+  let data = await si.mem();
+  return data;
+}
+
+// disk utilization
+async function getDiskInfo() {
+  let data = await si.diskLayout();
+  return data;
+}
+
+async function background() {
   const filePath = process.env.dataFilePath;
   let rawData = fs.readFileSync(filePath, "utf8");
   let parsedData = JSON.parse(rawData);
 
   const date = new Date();
-  const newBackground = {
-    date: date.toLocaleDateString(),
-    time: date.toLocaleTimeString(),
+
+  const currentLoad = await getCurrentLoad();
+  const memoryInfo = await getMemoryInfo();
+  const diskInfo = await getDiskInfo();
+  const timeStamp = date.toLocaleTimeString();
+
+  const backgroundStats = {
+    time: timeStamp,
+    stats: {
+      currentLoad,
+      memoryInfo,
+      diskInfo,
+    },
   };
-  parsedData.timeSeries.push(newBackground);
+  parsedData.timeSeries.push(backgroundStats);
 
   fs.writeFileSync(filePath, JSON.stringify(parsedData, null, 2), "utf8");
-};
-
+}
+background();
 setInterval(background, 1000 * 60); // run once a minute
-
-
 `;
 function spawnBackgroundProcess(dataFilePath) {
     return __awaiter(this, void 0, void 0, function* () {
