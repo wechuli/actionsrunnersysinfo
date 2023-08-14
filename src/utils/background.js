47b208bafb2860657a64ee4ac98c4e218f71247b
wechuli <wechulipaul@yahoo.com>
2023-08-14 08:46:36 +0300

    create code to collect stats

--- src/utils/background.js
+++ src/utils/background.js
@@ -1,18 +1,50 @@
 const fs = require("fs");
+const si = require("systeminformation");
 
-const background = () => {
+async function getCurrentLoad() {
+  let data = await si.currentLoad();
+  let importantLoadInfo = {
+    currentLoad: data.currentLoad,
+    currentLoadUser: data.currentLoadUser,
+    currentLoadSystem: data.currentLoadSystem,
+  };
+  return importantLoadInfo;
+}
+
+async function getMemoryInfo() {
+  let data = await si.mem();
+  return data;
+}
+
+// disk utilization
+async function getDiskInfo() {
+  let data = await si.diskLayout();
+  return data;
+}
+
+async function background() {
   const filePath = process.env.dataFilePath;
   let rawData = fs.readFileSync(filePath, "utf8");
   let parsedData = JSON.parse(rawData);
 
   const date = new Date();
-  const newBackground = {
-    date: date.toLocaleDateString(),
-    time: date.toLocaleTimeString(),
+
+  const currentLoad = await getCurrentLoad();
+  const memoryInfo = await getMemoryInfo();
+  const diskInfo = await getDiskInfo();
+  const timeStamp = date.toLocaleTimeString();
+
+  const backgroundStats = {
+    time: timeStamp,
+    stats: {
+      currentLoad,
+      memoryInfo,
+      diskInfo,
+    },
   };
-  parsedData.timeSeries.push(newBackground);
+  parsedData.timeSeries.push(backgroundStats);
 
   fs.writeFileSync(filePath, JSON.stringify(parsedData, null, 2), "utf8");
-};
-
+}
+background();
 setInterval(background, 1000 * 60); // run once a minute
