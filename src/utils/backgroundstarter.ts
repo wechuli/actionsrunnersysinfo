8af068735e87daa8b9f31f32035282af2cfbfef5
wechuli <wechulipaul@yahoo.com>
2023-08-14 09:07:50 +0300

    remove si information

--- src/utils/backgroundstarter.ts
+++ src/utils/backgroundstarter.ts
@@ -5,28 +5,28 @@ const path = process.cwd() + "/background.js";
 
 const js_content = `
 const fs = require("fs");
-const si = require("systeminformation");
+// const si = require("systeminformation");
 
-async function getCurrentLoad() {
-  let data = await si.currentLoad();
-  let importantLoadInfo = {
-    currentLoad: data.currentLoad,
-    currentLoadUser: data.currentLoadUser,
-    currentLoadSystem: data.currentLoadSystem,
-  };
-  return importantLoadInfo;
-}
+// async function getCurrentLoad() {
+//   let data = await si.currentLoad();
+//   let importantLoadInfo = {
+//     currentLoad: data.currentLoad,
+//     currentLoadUser: data.currentLoadUser,
+//     currentLoadSystem: data.currentLoadSystem,
+//   };
+//   return importantLoadInfo;
+// }
 
-async function getMemoryInfo() {
-  let data = await si.mem();
-  return data;
-}
+// async function getMemoryInfo() {
+//   let data = await si.mem();
+//   return data;
+// }
 
-// disk utilization
-async function getDiskInfo() {
-  let data = await si.diskLayout();
-  return data;
-}
+// // disk utilization
+// async function getDiskInfo() {
+//   let data = await si.diskLayout();
+//   return data;
+// }
 
 async function background() {
   const filePath = process.env.dataFilePath;
@@ -35,18 +35,18 @@ async function background() {
 
   const date = new Date();
 
-  const currentLoad = await getCurrentLoad();
-  const memoryInfo = await getMemoryInfo();
-  const diskInfo = await getDiskInfo();
+  // const currentLoad = await getCurrentLoad();
+  // const memoryInfo = await getMemoryInfo();
+  // const diskInfo = await getDiskInfo();
   const timeStamp = date.toLocaleTimeString();
 
   const backgroundStats = {
     time: timeStamp,
-    stats: {
-      currentLoad,
-      memoryInfo,
-      diskInfo,
-    },
+    // stats: {
+    //   currentLoad,
+    //   memoryInfo,
+    //   diskInfo,
+    // },
   };
   parsedData.timeSeries.push(backgroundStats);
 
@@ -54,6 +54,7 @@ async function background() {
 }
 background();
 setInterval(background, 1000 * 60); // run once a minute
+
 `;
 
 export async function spawnBackgroundProcess(
