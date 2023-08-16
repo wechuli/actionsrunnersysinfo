e09ad8d77f53a90345bd3fb81131bac224412005
wechuli <wechulipaul@yahoo.com>
2023-08-16 08:44:15 +0300

    implement stats logic

--- src/utils/background.js
+++ src/utils/background.js
@@ -1,27 +1,5 @@
 const fs = require("fs");
 const { execSync } = require("child_process");
-// const si = require("systeminformation");
-
-// async function getCurrentLoad() {
-//   let data = await si.currentLoad();
-//   let importantLoadInfo = {
-//     currentLoad: data.currentLoad,
-//     currentLoadUser: data.currentLoadUser,
-//     currentLoadSystem: data.currentLoadSystem,
-//   };
-//   return importantLoadInfo;
-// }
-
-// async function getMemoryInfo() {
-//   let data = await si.mem();
-//   return data;
-// }
-
-// // disk utilization
-// async function getDiskInfo() {
-//   let data = await si.diskLayout();
-//   return data;
-// }
 
 async function background() {
   const filePath = process.env.dataFilePath;
@@ -31,20 +9,43 @@ async function background() {
   execSync("npm install systeminformation");
   const si = require("systeminformation");
 
+  // cpu utilization
+  async function getCurrentLoad() {
+    let data = await si.currentLoad();
+    let importantLoadInfo = {
+      currentLoad: data.currentLoad,
+      currentLoadUser: data.currentLoadUser,
+      currentLoadSystem: data.currentLoadSystem,
+    };
+    return importantLoadInfo;
+  }
+
+  // memory utilization
+  async function getMemoryInfo() {
+    let data = await si.mem();
+    return data;
+  }
+
+  // disk utilization
+  async function getDiskInfo() {
+    let data = await si.diskLayout();
+    return data;
+  }
+
   const date = new Date();
 
-  // const currentLoad = await getCurrentLoad();
-  // const memoryInfo = await getMemoryInfo();
-  // const diskInfo = await getDiskInfo();
+  const currentLoad = await getCurrentLoad();
+  const memoryInfo = await getMemoryInfo();
+  const diskInfo = await getDiskInfo();
   const timeStamp = date.toLocaleTimeString();
 
   const backgroundStats = {
     time: timeStamp,
-    // stats: {
-    //   currentLoad,
-    //   memoryInfo,
-    //   diskInfo,
-    // },
+    stats: {
+      currentLoad,
+      memoryInfo,
+      diskInfo,
+    },
   };
   parsedData.timeSeries.push(backgroundStats);
 
--- src/utils/backgroundstarter.ts
+++ src/utils/backgroundstarter.ts
@@ -6,28 +6,6 @@ const path = process.cwd() + "/background.js";
 const js_content = `
 const fs = require("fs");
 const { execSync } = require("child_process");
-// const si = require("systeminformation");
-
-// async function getCurrentLoad() {
-//   let data = await si.currentLoad();
-//   let importantLoadInfo = {
-//     currentLoad: data.currentLoad,
-//     currentLoadUser: data.currentLoadUser,
-//     currentLoadSystem: data.currentLoadSystem,
-//   };
-//   return importantLoadInfo;
-// }
-
-// async function getMemoryInfo() {
-//   let data = await si.mem();
-//   return data;
-// }
-
-// // disk utilization
-// async function getDiskInfo() {
-//   let data = await si.diskLayout();
-//   return data;
-// }
 
 async function background() {
   const filePath = process.env.dataFilePath;
@@ -37,20 +15,43 @@ async function background() {
   execSync("npm install systeminformation");
   const si = require("systeminformation");
 
+  // cpu utilization
+  async function getCurrentLoad() {
+    let data = await si.currentLoad();
+    let importantLoadInfo = {
+      currentLoad: data.currentLoad,
+      currentLoadUser: data.currentLoadUser,
+      currentLoadSystem: data.currentLoadSystem,
+    };
+    return importantLoadInfo;
+  }
+
+  // memory utilization
+  async function getMemoryInfo() {
+    let data = await si.mem();
+    return data;
+  }
+
+  // disk utilization
+  async function getDiskInfo() {
+    let data = await si.diskLayout();
+    return data;
+  }
+
   const date = new Date();
 
-  // const currentLoad = await getCurrentLoad();
-  // const memoryInfo = await getMemoryInfo();
-  // const diskInfo = await getDiskInfo();
+  const currentLoad = await getCurrentLoad();
+  const memoryInfo = await getMemoryInfo();
+  const diskInfo = await getDiskInfo();
   const timeStamp = date.toLocaleTimeString();
 
   const backgroundStats = {
     time: timeStamp,
-    // stats: {
-    //   currentLoad,
-    //   memoryInfo,
-    //   diskInfo,
-    // },
+    stats: {
+      currentLoad,
+      memoryInfo,
+      diskInfo,
+    },
   };
   parsedData.timeSeries.push(backgroundStats);
 
@@ -58,8 +59,6 @@ async function background() {
 }
 background();
 setInterval(background, 1000 * 60); // run once a minute
-
-
 
 `;
 
@@ -82,9 +81,3 @@ export async function spawnBackgroundProcess(
   child.unref();
   return PID;
 }
-
-// spawnBackgroundProcess()
-//   .then((pid) => console.log(pid))
-//   .catch((err) => console.log(err));
-
-// create a file called background.js in the current working directory
